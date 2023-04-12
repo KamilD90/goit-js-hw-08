c52e20a533e64e6b5338830b8b33f6f024b35670
KamilD90 <117952413+KamilD90@users.noreply.github.com>
2023-04-12 23:10:14 +0200

    zad2 problems

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+// import Vimeo from '../../node_modules/@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+const savePlayTime = data => {
+  const currentTime = data.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+};
+const timeUpdate = player.on('timeupdate', savePlayTime);
+const saveTimeEverySecond = throttle(timeUpdate(data), 1000);
+
+window.onload = function () {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime) {
+    player.setCurrentTime(savedTime);
+  }
+};
