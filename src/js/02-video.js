a83eee88d5fdfb93a3c709fbb5d59d7b3c084c0d
KamilD90 <117952413+KamilD90@users.noreply.github.com>
2023-04-16 19:42:46 +0200

    finito

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,11 +5,15 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 const savePlayTime = data => {
-  const currentTime = data.seconds;
+  // console.log(data); // wypisanie danych do konsoli
+  const currentTime = data.seconds; // current time of video playing
   localStorage.setItem('videoplayer-current-time', currentTime);
 };
-const timeUpdate = player.on('timeupdate', savePlayTime);
-const saveTimeEverySecond = throttle(timeUpdate(data), 1000);
+
+const throttledSaveTime = throttle(savePlayTime, 1000);
+
+// player.on('timeupdate', savePlayTime); // zwykla aktualizacja czasu odtwarzania co wywoałenie eventu timeupdate
+player.on('timeupdate', throttledSaveTime); // wywolanie metody throttledSaveTime co minimum 1 sekunda (1000ms) (throttle ogranicza nam wywołanie metody raz na x czasu)
 
 window.onload = function () {
   const savedTime = localStorage.getItem('videoplayer-current-time');
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,56 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form');
+const emailProvided = document.querySelector("[name='email']");
+const messageWritten = document.querySelector("[name='message']");
+//mam złapane elementy z kodu HTML, terazpotrzeba funkcji która zapisze nam wartości wprowonycadzh pól do localStorage
+function saveForm() {
+  const formValues = {
+    email: emailProvided.value,
+    message: messageWritten.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formValues));
+  // localStorage jest mechanizmem przetrzymywania danych, który obsługuje tylko dane typu string. Aby więc zapisać obiekt JavaScript w localStorage, musimy go przekształcić na format stringowy. Metoda JSON.stringify() służy do przekształcania obiektów JavaScript na ciągi znaków w formacie JSON, który jest tekstem i może być zapisany w localStorage.
+}
+
+// nastęnie tworzymy funkcje odczytu zapisanych w localStorage wartości
+
+function loadFormValues() {
+  const formValues = JSON.parse(localStorage.getItem('feedback-form-state'));
+  // Podczas odczytu danych z localStorage, używamy metody JSON.parse(), aby przekształcić ciąg znaków JSON zapisany w localStorage na obiekt JavaScript.
+  if (formValues) {
+    emailProvided.value = formValues.email;
+    messageWritten.value = formValues.message;
+  }
+}
+//funkcja wywoływana po wysłaniu formularza, po kliknięciu na button submitt
+
+const handleSubmit = event => {
+  //   zapobiega domyślnemu wyczyszczeniu formularza tuż po naciśnięciu submit
+  //   debugger;
+  //jak chcesz sie zatrzymać w kodzie możesz sprawdzić jaki jest stan
+
+  event.preventDefault();
+  //JEBANE LITERÓWKI default nie deafult
+
+  //teraz wyrzuć do konsoli obiekt z polami email i message
+  console.log({
+    email: emailProvided.value,
+    message: messageWritten.value,
+  });
+  //teraz wyczyść obiekt feedback-form-state z pamięci przeglądarki
+  localStorage.removeItem('feedback-form-state');
+  emailProvided.value = '';
+  messageWritten.value = '';
+};
+
+//dodamy eventlistner na pola email i textarea
+//Zdarzenie input w JavaScript jest wywoływane, gdy użytkownik dokonuje zmiany w elemencie formularza (np. wprowadza tekst do pola tekstowego, wybiera opcję z listy rozwijanej itp.).
+// Kiedy użytkownik wprowadza dane, zdarzenie input jest wywoływane w czasie rzeczywistym, w odróżnieniu od zdarzenia change, które jest wywoływane dopiero po opuszczeniu pola formularza.
+emailProvided.addEventListener('input', throttle(saveForm, 500));
+messageWritten.addEventListener('input', throttle(saveForm, 500));
+
+document.addEventListener('DOMContentLoaded', loadFormValues);
+//DOMContentLoaded to zdarzenie, które jest wywoływane, gdy przeglądarka w pełni załadowała dokument HTML, a następnie wygenerowała drzewo DOM (Document Object Model) dla całej strony.
+
+form.addEventListener('submit', handleSubmit);
